68b5caeb8d45e8356d75f8f18fbc457d6e5d6d7b
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-03-14 12:16:05 +0900

    [Platinum V] Title: 버블 소트, Time: 116 ms, Memory: 7176 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void merge_sort(int S,int E);
+vector<int> arr;
+vector<int> tmp;
+long result = 0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin>>N;
+	
+	tmp = vector<int>(N,0);
+	for (int i = 0; i < N; i++) {			
+		int num;
+		cin>>num;
+		arr.push_back(num);
+	}
+
+	merge_sort(0,N-1);
+	cout<<result;
+	
+
+	return 0;
+}
+
+void merge_sort(int S, int E) {
+	if (E==S) {
+		return;
+	}
+
+	int M = S+(E-S)/2;
+	merge_sort(S,M);
+	merge_sort(M+1,E);
+
+	for (int i = S; i <= E; i++) {
+		tmp[i] = arr[i];
+	}
+
+	int k=S;
+	int index1 = S;
+	int index2 = M+1;
+
+	while (index1 <= M && index2 <= E) {
+		if (tmp[index1] > tmp[index2]) {
+			arr[k] = tmp[index2];
+			result += index2 - k;
+			k++; index2++;
+		}
+		else {
+			arr[k] = tmp[index1];
+			k++; index1++;
+		}
+	}
+
+	while (index1 <= M) {
+		arr[k] = tmp[index1];
+		k++; index1++;
+	}
+
+	while (index2 <= E) {
+		arr[k] = tmp[index2];
+		k++; index2++;
+	}
+
+}
