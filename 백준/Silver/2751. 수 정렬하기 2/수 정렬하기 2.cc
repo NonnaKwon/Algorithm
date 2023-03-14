eed09f008e22ebdf11897f96f3eef80d411be613
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-03-14 11:47:20 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 324 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,23 +1,70 @@
 #include <iostream>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
+void merge_sort(int S,int E);
+vector<int> arr;
+vector<int> tmp;
 int main() {
-    int n;
-    scanf("%d",&n);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int* arr=new int[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d",&arr[i]);
-    }
+	int N;
+	cin>>N;
+	
+	tmp = vector<int>(N,0);
+	for (int i = 0; i < N; i++) {			
+		int num;
+		cin>>num;
+		arr.push_back(num);
+	}
 
-    sort(arr,arr+n);
-    
-    for (int i = 0; i < n; i++) {
-        printf("%d\n",arr[i]);
-    }
+	merge_sort(0,N-1);
+	for (int i = 0; i < N; i++) {
+		cout<<arr[i]<<"\n";
+	}
+	
 
-    delete[] arr;
-    return 0;
+	return 0;
 }
 
+void merge_sort(int S, int E) {
+	if (E==S) {
+		return;
+	}
+
+	int M = S+(E-S)/2;
+	merge_sort(S,M);
+	merge_sort(M+1,E);
+
+	for (int i = S; i <= E; i++) {
+		tmp[i] = arr[i];
+	}
+
+	int k=S;
+	int index1 = S;
+	int index2 = M+1;
+
+	while (index1 <= M && index2 <= E) {
+		if (tmp[index1] > tmp[index2]) {
+			arr[k] = tmp[index2];
+			k++; index2++;
+		}
+		else {
+			arr[k] = tmp[index1];
+			k++; index1++;
+		}
+	}
+
+	while (index1 <= M) {
+		arr[k] = tmp[index1];
+		k++; index1++;
+	}
+
+	while (index2 <= E) {
+		arr[k] = tmp[index2];
+		k++; index2++;
+	}
+
+}
