73993a2fc0cb11c6664a97d76e6d8494b5d1a8c8
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-04-10 23:16:09 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,46 +1,44 @@
 #include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
 
 
-
 int main() {
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int tmp_size =0 ,result_size =0;
-    int n;
-    cin >> n;
+	int n;
+	cin>>n;
 
-    int* tmpArr = new int[n];
-    char* resultArr = new char[n*2];
+	vector<char> result;
+	stack<int> arr;
+	int count=1;
+	bool isSuccess=true;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin>>x;
+		while (count <= x) {
+			result.push_back('+');
+			arr.push(count);
+			count++;
+		}
+		if (arr.top() == x) {
+			arr.pop();
+			result.push_back('-');
+		}
+		else {
+			cout<<"NO"<<"\n";
+			isSuccess = false;
+			break;
+		}
+	}
 
-    int count = 1;
-    int input;
-    for (int i = 0; i < n; i++) {
-        cin>>input;
-        while (true) {
-            if (tmp_size != 0 && tmpArr[tmp_size-1] == input) {
-                resultArr[result_size] = '-';
-                tmpArr[tmp_size]=0;
-                result_size++; tmp_size--;
-                break;
-            }
-            else if (count > n) {
-                cout << "NO" << endl;
-                return 0;
-            }
-
-            resultArr[result_size] = '+';
-            tmpArr[tmp_size] = count;
-            result_size++; tmp_size++;
-            count++;
-        }
-    }
-
-    for (int i = 0; i < result_size; i++) {
-        cout<< resultArr[i]<<'\n';
-    }
-
-    return 0;
-
+	if (isSuccess) {
+		for (int i = 0; i < result.size(); i++) {
+			cout<<result[i]<<"\n";
+		}
+	}
+	return 0;
 }
