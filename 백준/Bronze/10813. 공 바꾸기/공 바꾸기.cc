b0dcaf07a122997ecc31a92fa5e4921b3d7cf3a2
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-04-12 23:23:06 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void swap(int a,int b);
+vector<int> arr;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N,M;
+	cin>>N>>M;
+	for (int i = 0; i <= N; i++) {
+		arr.push_back(i);
+	}
+	for (int i = 0; i < M; i++) {
+		int a,b;
+		cin>>a>>b;
+		swap(a,b);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		cout<<arr[i]<<" ";
+	}
+
+	return 0;
+}
+void swap(int a, int b) {
+	int tmp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = tmp;
+}
