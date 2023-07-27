0743928317115b6d317be5ea593f835af296784b
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-07-27 20:22:43 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2656 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -1,36 +1,53 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <cmath>
 using namespace std;
 
-struct cmp {
-	bool operator()(int a, int b) {
-		if(abs(a) != abs(b)) return abs(a)>abs(b);
-		return a>b;
+struct compare
+{
+	bool operator()(int& num1, int& num2)
+	{
+		if (abs(num1) == abs(num2))
+		{
+			return num1 > num2;
+		}
+		return abs(num1) > abs(num2);
 	}
 };
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	int N;
-	cin>>N;
-
-	priority_queue<int,vector<int>,cmp> q;
-	
-	for (int i = 0; i < N; i++) {
-		int x;
-		cin>>x;
-		if(x!=0) q.push(x);
-		else {
-			if (q.empty()) {
-				cout<<"0"<<"\n";
-			}
-			else {
-				cout<<q.top()<<"\n";
-				q.pop();
+	cin >> N;
+	priority_queue<int, vector<int>, compare> pq;
+	vector<int> result;
+	while (N--)
+	{
+		int num;
+		cin >> num;
+		if (num == 0)
+		{
+			if (pq.size() == 0) result.push_back(0);
+			else 
+			{
+				result.push_back(pq.top());
+				pq.pop();
 			}
 		}
+		else
+		{
+			pq.push(num);
+		}
 	}
-	return 0;
+
+	for (int i = 0; i < result.size(); i++)
+	{
+		cout << result[i] << '\n';
+	}
 }
+
+
