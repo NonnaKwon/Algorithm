0ba145439e5984b802f3bf87ff5e0c16707c399e
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-08-07 17:56:41 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,34 +1,34 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <queue>
 using namespace std;
 
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int n;
-    cin>>n;
-    int *arr = new int[n];
+	int N;
+	cin >> N;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		pq.push(num);
+	}
+	
+	int sum = 0;
+	int S = 0;
+	for (int i = 0; i < N; i++)
+	{
+		S += pq.top();
+		pq.pop();
+		sum += S;
+	}
 
-    for (int i = 0; i < n; i++) {
-        cin>>arr[i];
-    }
-
-    //정렬한다
-    sort(arr,arr+n);
-    //sum에 차례로 더한다
-    /*
-    for 0~n 이중중첩    
-*/
-
-    int sum=0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            sum+=arr[j];
-        }
-    }
-    cout<<sum<<endl;;
-    return 0;
+	cout << sum << "\n";
 }
+
 
