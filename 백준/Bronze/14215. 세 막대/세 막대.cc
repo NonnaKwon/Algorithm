690d128697aca25301b338495a3a36ed1ffe4223
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-08-30 21:03:19 +0900

    [Bronze III] Title: 세 막대, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int arr[3];
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + 3);
+	
+	if (arr[2] >= arr[0] + arr[1])
+	{
+		arr[2] = arr[0] + arr[1] - 1;
+	}
+
+	int result = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		result += arr[i];
+	}
+
+	cout << result << endl;
+	return 0;
+}
