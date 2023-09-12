afe69b18c52afd2e25ae003f8a02f17eceedf1a3
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-09-12 09:33:33 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    while(order>0)
+    {
+        if(order%10 == 3 || order%10 == 6||order%10 == 9)
+            answer++;
+        order /= 10;
+    }
+    return answer;
+}
