5f65eb7bcd01dc5f92f0697e33a9838c194855bb
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2023-11-13 17:50:52 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,24 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+
+
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
+	vector<long long> D;
 	int N;
-	cin>>N;
+	cin >> N;
 
-	long long B[1001];
-	B[0]=0;
-	B[1]=1;
-	B[2]=2;
-
-	for (int i = 3; i <= N; i++) {
-		B[i] = B[i-1]+B[i-2];
-		B[i] %= 10007;
+	D.resize(N + 1);
+	D[1] = 1;
+	D[2] = 2;
+	for (int i = 3; i <= N; i++)
+	{
+		D[i] = D[i - 1] + D[i - 2];
+		D[i] %= 10007;
 	}
 
-	cout<<B[N]<<"\n";
+	cout << D[N];
 	return 0;
 }
