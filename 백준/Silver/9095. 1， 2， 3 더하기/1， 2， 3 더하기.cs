326f21b6fab4e2c15771ec6691b6517d455371fb
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2024-10-20 00:19:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 48 ms, Memory: 5824 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cs
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cs
@@ -4,28 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CordingStudy_CS.Week11_CS
+namespace CordingStudy_CS.Week_12
 {
     internal class Program
     {
+        // DP 
         public static void Main(string[] args)
         {
-            int T = int.Parse(Console.ReadLine());
-            List<int> list = new List<int>();
-            list.Add(0);
-            list.Add(1);
-            list.Add(2);
-            list.Add(4);
-
+            // dp를 만든다.(인덱스가 n)
+            List<int> dp = new List<int>();
+            dp.Add(0);
+            dp.Add(1);
+            dp.Add(2);//2는 1+1, 2 두가지.
+            dp.Add(4);//3은 1+1+1,1+2,2+1,3
             for(int i=4;i<11;i++)
             {
-                //1. 이전 숫자의 경우에 +1 한 경우
-                list.Add(list[i - 1] + list[i - 2] + list[i - 3]);
+                int answer = dp[i - 1];
+                answer += dp[i - 2];
+                answer += dp[i - 3];
+                dp.Add(answer);
             }
+
+            //문제를 푼다.
+            int T = int.Parse(Console.ReadLine());
             while(T-->0)
             {
                 int n = int.Parse(Console.ReadLine());
-                Console.WriteLine(list[n]);
+                Console.WriteLine(dp[n]);
             }
         }
     }
